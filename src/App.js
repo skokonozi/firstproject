4e198838a01cebcff841b8c4be955bd419c10b36
skokonozi <sebi@newventures.al>
2022-10-27 17:09:24 +0200

    Multiple States

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import React from "react";
+import NewExpense from "./component/NewExpense/NewExpense";
 import Expenses from "./component/Expenses/Expenses";
 function App() {
   const expenses = [
@@ -33,7 +34,7 @@ function App() {
 
   return (
    <div>
-    <h2>Let's get started!</h2>
+    <NewExpense/>
     <Expenses items = {expenses} />
    </div>
   );
